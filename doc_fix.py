2c7efdb99c9dabfde1c5a7fd99d5aa4eff252c73
kpapdac <kpapdac@gmail.com>
2019-04-06 21:22:44 +0100

    Create doc_fix

---
+++ doc_fix.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = pd.read_json(r'C:\Users\Katerina\pandas\scripts\validation_errors.json')
+data.columns
+
+docstring = {}
+for i in data.columns:
+    if len(data[i]['errors']) > 0:
+        docstring[i] = data[i]['errors'][0][0]
+
+err_freq = {}
+for i in set(list(docstring.values())):
+    err_freq[i] = list(docstring.values()).count(i)
+
+a = [i for i in docstring.keys() if docstring[i] == 'GL08']
+#for i in data.columns:
+#    if docstring.get()
+data.head()
+
+
